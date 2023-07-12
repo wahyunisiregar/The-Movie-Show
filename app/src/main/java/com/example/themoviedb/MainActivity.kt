998cb1cf8167576faecdf9c4e256be16d7f12951
annisakhoi <annkhoiermy210@gmail.com>
2023-07-12 23:52:35 +0700

    bottomnavigation

--- app/src/main/java/com/example/themoviedb/MainActivity.kt
+++ app/src/main/java/com/example/themoviedb/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.themoviedb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.themoviedb.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -14,19 +16,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         viewPageAdapter = ViewPageAdapter(supportFragmentManager, lifecycle)
 
-        with(binding){
+        with(binding) {
             viewPager.adapter = viewPageAdapter
 
             TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-                when(position){
+                when (position) {
                     0 -> tab.text = "Movie"
                     1 -> tab.text = "TV"
                 }
             }.attach()
         }
 
+
+        val movieFragment = MovieFragment()
+        val tvFragment = TVFragment()
+
+
+        makeCurrentFragment(movieFragment)
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_movie -> makeCurrentFragment(movieFragment)
+                R.id.menu_tv -> makeCurrentFragment(tvFragment)
+            }
+
+            true
+
+
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fram_nav, fragment)
+            commit()
+        }
     }
 }
--- app/src/main/java/com/example/themoviedb/MovieFragment.kt
+++ app/src/main/java/com/example/themoviedb/MovieFragment.kt
@@ -16,6 +16,9 @@ import retrofit2.Callback
 import retrofit2.Response
 
 
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
 class MovieFragment : Fragment() {
     private val movies = arrayListOf<Movie>()
 
@@ -54,4 +57,25 @@ class MovieFragment : Fragment() {
         rv_movies_list.layoutManager = LinearLayoutManager(this.context)
         rv_movies_list.adapter = MovieAdapter(movies)
     }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MovieFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
 }
